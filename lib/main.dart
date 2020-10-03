32980d4c6f3e6d5849f0250362f1dd8c3c3a5d8f
BaLiKfromUA <yuhimenko.valentin@gmail.com>
2020-10-03 15:17:05 +0300

    stolen from tutor -- easy win

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import './screens/list_page.dart';
 
 void main() {
   runApp(Goli());
 }
 
 class Goli extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+          primaryColor: Color.fromRGBO(58, 66, 86, 1.0), fontFamily: 'Raleway'),
+      home: new ListPage(title: 'Lessons'),
+    );
   }
 }
